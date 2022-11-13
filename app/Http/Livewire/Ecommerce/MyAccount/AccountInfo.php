a7c1fe307b27ddef6ca1d2c52a28604f5f9b1398
Edwin Serna <edwinflow888@gmail.com>
2022-11-13 11:24:41 -0500

    Se agrega la pagina de gestión del usuario

--- app/Http/Livewire/Ecommerce/MyAccount/AccountInfo.php
+++ app/Http/Livewire/Ecommerce/MyAccount/AccountInfo.php
@@ -2,14 +2,56 @@
 
 namespace App\Http\Livewire\Ecommerce\MyAccount;
 
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
 class AccountInfo extends Component
 {
     public bool $active = false;
 
+    public $user;
+    public $new_password;
+    public $current_password;
+    public $new_password_confirmation;
+
+    protected $validationAttributes = [
+        'new_password' => 'contraseña',
+    ];
+
+    protected function rules()
+    {
+        return [
+            'user.name' => 'sometimes|min:6|max:100',
+            'user.email' => 'sometimes|unique:users,email,' . $this->user->id,
+            'current_password' => 'nullable|current_password',
+            'new_password' => 'nullable|min:6|confirmed',
+        ];
+    }
+
+    public function mount()
+    {
+        $this->user = auth()->user();
+    }
+
     public function render()
     {
         return view('livewire.ecommerce.my-account.account-info');
+    }
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function submit()
+    {
+        $data = $this->validate();
+
+        if (isset($data['new_password'])) {
+            $this->user->password = Hash::make($data['new_password']);
+        }
+
+        $this->user->save();
+        session()->flash('success_message', _('Changes Successfully'));
     }
 }
--- resources/views/livewire/ecommerce/my-account/account-info.blade.php
+++ resources/views/livewire/ecommerce/my-account/account-info.blade.php
@@ -2,36 +2,52 @@
     <div class="myaccount-content">
         <h3>Account Details</h3>
         <div class="account-details-form">
-            <form action="#">
+            <form wire:submit.prevent="submit">
                 <div class="row">
-                    <div class="col-lg-6 col-12 mb--30">
-                        <input id="first-name" placeholder="First Name" type="text" />
-                    </div>
-                    <div class="col-lg-6 col-12 mb--30">
-                        <input id="last-name" placeholder="Last Name" type="text" />
+                    <div class="col-12 mb--30">
+                        <input id="full-name" placeholder="Full Name" type="text" wire:model="user.name" />
+                        @error('user.name')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
                     </div>
                     <div class="col-12 mb--30">
-                        <input id="display-name" placeholder="Display Name" type="text" />
+                        <input id="email" placeholder="Email Address" type="email" wire:model="user.email" />
+                        @error('user.email')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
                     </div>
-                    <div class="col-12 mb--30">
-                        <input id="email" placeholder="Email Address" type="email" />
-                    </div>
+
                     <div class="col-12 mb--30">
                         <h4>Password change</h4>
                     </div>
+
                     <div class="col-12 mb--30">
-                        <input id="current-pwd" placeholder="Current Password" type="password" />
+                        <input id="current_password" placeholder="Current Password" type="password"
+                            wire:model="current_password" />
+                        @error('current_password')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
                     </div>
                     <div class="col-lg-6 col-12 mb--30">
-                        <input id="new-pwd" placeholder="New Password" type="password" />
+                        <input id="new_password" placeholder="New Password" type="password" wire:model="new_password" />
+                        @error('new_password')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
                     </div>
                     <div class="col-lg-6 col-12 mb--30">
-                        <input id="confirm-pwd" placeholder="Confirm Password" type="password" />
+                        <input id="new_password_confirmation" placeholder="Confirm Password" type="password"
+                            wire:model="new_password_confirmation" />
+                        @error('new_password_confirmation')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
                     </div>
                     <div class="col-12">
-                        <button class="btn btn--primary">
+                        <button type="submit" class="btn btn--primary">
                             Save Changes
                         </button>
+                    </div>
+                    <div class="col-12 pt-3">
+                        @include('partials.alerts.succes-or-errors')
                     </div>
                 </div>
             </form>
